fe8680d8ab0b095e3192daca621113dcb83669da
Erin <erinharrington12@yahoo.com>
2024-01-30 16:17:42 -0600

    More Create 3 Documentation

--- shared/client/doc/namespacestd.js
+++ shared/client/doc/namespacestd.js
@@ -1,4 +1,5 @@
 var namespacestd =
 [
-    [ "hash< kipr::log::Level >", "structstd_1_1hash_3_01kipr_1_1log_1_1Level_01_4.html", "structstd_1_1hash_3_01kipr_1_1log_1_1Level_01_4" ]
+    [ "hash< kipr::log::Level >", "structstd_1_1hash_3_01kipr_1_1log_1_1Level_01_4.html", "structstd_1_1hash_3_01kipr_1_1log_1_1Level_01_4" ],
+    [ "iterator_traits<capnp::_::IndexingIterator<Container, Element>>", "structstd_1_1hash_3_01kipr_1_1log_1_1Level_01_4.html", "structstd_1_1hash_3_01kipr_1_1log_1_1Level_01_4" ],
 ];
---
+++ shared/client/doc/twist_8h.js
@@ -0,0 +1,5 @@
+var twist_8h =
+[
+    [ "linear_x", "twist_8h.html#af0c555a8582de02b428aa7b11735a45d", null ],
+    [ "angular_z", "twist_8h.html#a541442824a656f9c820631f4b8adf441", null ]
+];
